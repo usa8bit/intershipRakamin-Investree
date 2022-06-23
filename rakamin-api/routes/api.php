0330039159e67f00ce36dec58ff44be4e2553316
usa8bit <usa8bit@gmail.com>
2022-06-23 23:19:01 +0700

    menambah project laravel passport

---
+++ rakamin-api/routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\PostController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::prefix('v1')->group(function () {
+    Route::post('login', [LoginController::class, 'login']);
+    Route::post('register', [LoginController::class, 'register']);
+
+    Route::middleware('auth:api')->group(function () {
+        Route::resource('posts', PostController::class);
+    });
+});
